7d5fcfafbfaee2a64e2d62369165d227c121d218
Pendragonz <danielparekh@protonmail.com>
2020-03-08 18:59:34 +0000

    added INSECURE auth and bsaic parameter checking

--- api.py
+++ api.py
@@ -1,12 +1,85 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+from flask_restful import Api, Resource
+
 
 app = Flask(__name__)
+auth=HTTPBasicAuth()
+api=Api(app, prefix="/api/")
 
+userRegistered=False
+testnetAppRunning=False
+mainnetAppRunning=False
+
+users = {}
 
 @app.route('/')
+@app.route('/home')
+@app.route('/index')
 def index():
 	return 'Hello World!'
 
+@app.route('/register/<username>/<password>')
+def register(username, password):
+	global users, userRegistered
+	if userRegistered==True:
+		return 'Admin already registered.'
+	else:
+		users={username:password}
+		userRegistered=True
+		return 'user registered! welcome ' + username
+
+@auth.verify_password
+def verify_password(username, password):
+	global users
+	if username in users:
+		return True
+	else:
+		return False
+
+
+@app.route('/private')
+@auth.login_required
+def privvy():
+	return 'welcome admin'
+
+
+@app.route('/run/testnet')
+@auth.login_required
+def testnetService():
+	global testnetAppRunning, mainnetAppRunning
+	if testnetAppRunning==True:
+        	return 'Testnet App already running.'
+	elif mainnetAppRunning==True:
+		return 'Mainnet App already running. Please terminate before retrying.'
+	else:
+		testnetAppRunning=True
+		return 'Ok Ill run that script in just a jiffy'
+
+
+@app.route('/run/mainnet/<int:temp>/<int:humid>/<int:interval>')
+@auth.login_required
+def mainnetService(temp, humid, interval):
+	global testnetAppRunning, mainnetAppRunning
+	if testnetAppRunning==True:
+		return 'Testnet App already running.'
+	elif mainnetAppRunning==True:
+		return 'mainnet app is already running'
+	else:
+		#Check Inputs
+		if temp < 0 or temp >= 2:
+			return 'temp variable out of bounds. Should be 0 or 1'
+		if humid < 0 or humid >= 2:
+			return 'humid variable out of bounds. Should be 0 or 1'
+		if interval <= 1:
+			return 'interval can not be less than 2 seconds'
+
+
+		mainnetAppRunning=True
+		return 'running app on mainnet'
+
+
 if __name__ == '__main__':
 	app.run(port=5000, host='0.0.0.0', debug=True)
+
 
