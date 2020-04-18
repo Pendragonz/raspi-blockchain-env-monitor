38cce12dacfa6d3ef04bae4383ac9b1f36dac314
Pendragonz <danielparekh@protonmail.com>
2020-04-18 01:48:54 +0100

    format time properly to ensure 00;00;00

--- read.py
+++ read.py
@@ -1,6 +1,7 @@
 
 import Adafruit_DHT
 import time
+import datetime
 from decimal import Decimal
 import os.path
 import subprocess
@@ -11,9 +12,10 @@ import sys
 DHT_SENSOR=Adafruit_DHT.DHT22
 DHT_PIN=4
 
-#Set up run conditions based on parameters TODO
+#Set up run conditions based on parameters
 if len(sys.argv) > 1:
 	INTERVAL=int(sys.argv[1])
+	print("read.py interval set: " + str(INTERVAL))
 else:
 	INTERVAL=20
 
@@ -48,18 +50,25 @@ def mainLoop():
 			avg_temp=running_total_temp/num_readings
 			avg_humid=running_total_humid/num_readings
 
-			#Limit avgs to  2dp
+			#Round avgs to  2dp
 			avg_temp=round(Decimal(avg_temp), 2)
 			avg_humid=round(Decimal(avg_humid), 2)
+
+			#set avgs to dp
+			avg_temp="%.2f"%float(avg_temp)
+			avg_humid="%.2f"%float(avg_humid)
+
 			#format date time to;  Month/Day;hour:minute:second e.g. 10/12;14:45:12
-			t_=time.localtime()[1:6]
-			dt=str(t_[1]) + "/"+ str(t_[0]) + ";" + str(t_[2])+":"+str(t_[3])+":"+str(t_[4])
+			#t_=time.localtime()[1:6]
+			#dt=str(t_[1]) + "/"+ str(t_[0]) + ";" + str(t_[2])+":"+str(t_[3])+":"+str(t_[4])
+			dt="{0:%d-%m;%H:%M:%S}".format(datetime.datetime.now())
+
 
 			#Keep trying to add it to the db
-			while writeToDB(avg_temp, avg_humid, str(dt)) is not True:
+			while writeToDB(avg_temp, avg_humid, dt) is not True:
 				time.sleep(5)
 
-			print('vals written to db')
+			print('vals written to db' + " interval: " + str(INTERVAL))
 
 			#Reset Loop Variables
 			running_total_temp=0
