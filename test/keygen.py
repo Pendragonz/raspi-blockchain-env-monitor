a34e33f2d3e0c4c10eae4756dabb7e938d8a4fdc
Daniel <danielparekh@protonmail.com>
2020-05-07 01:38:17 +0100

    modified so it saves data in the same format as api.py>>>keys.txt

--- test/keygen.py
+++ test/keygen.py
@@ -4,8 +4,10 @@ from stellar_sdk import Keypair
 keypair = Keypair.random()
 
 
+str="TESTINGKEY, " + keypair.public_key + ", " + keypair.secret
+
 
 with open("testing_keys.txt", "w") as f:
-	f.write(keypair.secret)
+	f.write(str)
 
 print(keypair.public_key)
